88876cbbe428087dee33fb729c291de457ec33b5
Robert Roman <rroman6174@gmail.com>
2021-01-21 09:18:11 -0500

    Move hook interfaces to dedicated api file.

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,22 +1,4 @@
-/**
- * Provides an interface for this extension's API.
- * If the onPreCommit or onPreRevert functions throw an error, the action
- * will be aborted.
- *
- * @export
- * @interface SVNHook
- */
-export interface SVNHook {
-  onPreCommit?(documents: string[]): Promise<void>;
-  onPostCommit?(documents: string[]): Promise<void>;
-  onPreRevert?(documents: string[]): Promise<void>;
-  onPostRevert?(documents: string[]): Promise<void>;
-  onUpdate?(): Promise<void>;
-}
-
-export interface SVNHookApi {
-  registerHook(hook: SVNHook): void;
-}
+import { SVNHook } from "./hooks/svnHookApi";
 
 const registeredHooks: SVNHook[] = [];
 
